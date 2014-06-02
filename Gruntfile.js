be725cd2e877ddc1e2f6a518699ae232959e10db
Jordy Zagdanski <jayzed@gmail.com>
2014-06-01 22:10:28 -0400

    adding bootstrap sass 3.1.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,23 +2,35 @@ module.exports = function(grunt) {
 
     // 1. All configuration goes here 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+      pkg: grunt.file.readJSON('package.json'),
 
-        watch: {
-            scripts: {
-                files: ['js/*.js'],
-                options: {
-                    spawn: false,
-                },
-            } 
+      watch: {
+        scripts: {
+          files: ['js/*.js'],
+          options: {
+            spawn: false,
+          },
+        } 
+      },
+
+      sass: {
+        dist: {
+          options: {                       // Target options
+            style: 'expanded'
+          },
+          files: {                         
+            'css/main.css': 'css/main.scss'
+          }
         }
+      }
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', ['watch', 'sass']);
 
-};
+  };
--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 This is my boilerplate to create boilerplate sites. It is based off of [HTML5 Boilerplate](http://html5boilerplate.com)
 
 ## To-Do
-- Add Bootstrap SASS
 - Add Bourbon
 - Add Font-Awesome
 - Grunt
