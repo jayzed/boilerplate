cd6e5cc3934075c13dbbd8dbbf3abc8eb55fe790
Jordy Zagdanski <jayzed@gmail.com>
2014-06-18 00:13:57 -0400

    moving html head to includes file

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <!DOCTYPE html>
 <html>
-    <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <title>Boilerplate</title>
-      <meta name="description" content="">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      
-      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" >
-      <link rel="stylesheet" href="css/build/production.css">
-      
-      <script src="js/build/production.js"></script>
-    </head>
+  
+  <?php include "include/head.php" ?>
 
     <body>
         <p>Hello world! This is My Boilerplate.</p>
